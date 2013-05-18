34e310d06b4ace52a8f081c4e56ec4c1103232d7
Prasanth <prasanth365@outlook.com>
2013-05-19 01:20:46 +0530

    Hello World Example JRuby FX - Initial Commit

--- jrubyfx_hello_world.rb
+++ jrubyfx_hello_world.rb
@@ -6,15 +6,18 @@ class HelloWorldExp < JRubyFX::Application
 
 		with(stage,title:"JRuby FX Example",width:300,height:300) do 
 
-			g = GridPane.new
-			g.alignment = Pos::CENTER			
 			label = Label.new()
 			label.text = "Click here to speak"
 			label.font = Font.new("Calibri",20)
-			g.add(label, 0, 0);
+			
 			b = Button.new("Click me")
-			sce = Scene.new(g)			
+
+			g = GridPane.new
+			g.alignment = Pos::CENTER
+			g.add(label, 0, 0);
 			g.add(b,0,1)
+
+			sce = Scene.new(g)			
 			stage.scene = sce	
 
 			b.set_on_action do
@@ -24,7 +27,8 @@ class HelloWorldExp < JRubyFX::Application
 				label.textFill = linear_gradient(0,0,0,1, true, :no_cycle, [stop(0, :cyan), stop(1, :dodger_blue)])
 				label.set_effect(drop_shadow(color: :dodger_blue, radius: 25,spread: 0.25))
 				b.opacity = 0
-			end				
+			end	
+
 		end
 
 		stage.show()
