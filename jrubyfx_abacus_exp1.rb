f303e463653882b3f557513cdecce628260b83c3
Prasanth <prasanth365@outlook.com>
2013-05-30 20:18:17 +0530

    Sample - Circle 2 JRubyFX

--- jrubyfx_abacus_exp1.rb
+++ jrubyfx_abacus_exp1.rb
@@ -2,24 +2,48 @@ require "jrubyfx"
 
 class AbacusExp1 < JRubyFX::Application
 
+	@@ROW_COUNT = 10
+	@@COL_COUNT = 10
+	@@RADIUS = 20
+	@@DIAMETER = 2 * @@RADIUS
+	@@SCENE_WIDTH = @@COL_COUNT * @@DIAMETER
+	@@SCENE_HEIGHT = @@ROW_COUNT * @@DIAMETER
+	@@TRANS_DIST = 100
+
+	def create_circle(row,column)
+
+		c = circle(radius:@@RADIUS-1, centerX:(@@RADIUS + (column * @@DIAMETER)), centerY:(@@RADIUS + (row * @@DIAMETER))) do
+
+			set_on_mouse_clicked do |event|
+
+					c.translateX = @@TRANS_DIST				
+
+			end
+
+		end
+
+	end
+
 	def start(stage)
 
-		with(stage,title:"JRubyFX Abacus Experiment 1",width:400,height:400) do
+		with(stage,title:"JRubyFX Abacus Experiment 1", width:@@SCENE_WIDTH + @@TRANS_DIST + @@RADIUS, height:@@SCENE_HEIGHT) do
 
 			p = Pane.new
 
-			c = Circle.new()	
-			c.radius = 20
-			c.centerX = 20
-			c.centerY = 20
+			@@ROW_COUNT.times do |x|
+				
+				@@COL_COUNT.times do |y|
 
-			p.add(c);
+					p.add(AbacusExp1.new().create_circle(x,y))
 
-			sce = Scene.new(p)	
-			stage.scene = sce
+				end
+
+			end
+			
+			stage.scene = Scene.new(p)
 			
 		end
-		
+
 		stage.show()
 
 	end
